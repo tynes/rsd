5223db4617d2237d3c98e7640ff5d543244987cb
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2019-04-03 00:23:30 -0500

    beginning string type for Address

--- src/primitives/address.rs
+++ src/primitives/address.rs
@@ -1,14 +1,50 @@
 use blake2::Blake2b;
+use bech32::Bech32; // https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki#bech32
+use std::str::FromStr;
 
 pub struct Address {
     pub version: u32,
-    pub hash: Blake2b
+    pub hash: Blake2b,
+    // Eventually will be type 'network'
+    pub network: String
 }
 
-impl From<String> for Address {
-    fn from(item: i32) -> Self {
-        Number { value: item }
+// Maybe do this https://doc.rust-lang.org/std/str/trait.FromStr.html
+impl From<&str> for Address {
+    fn from(data: &str) -> Self {
+        // bech32 --> [ addr.hrp | separator: 1 | version = addr.data[0] | remainder of addr.data ]
+        // Example: "ts1qq79hzunlkj50fvm7rxg3xetx4kml4e0am43htk"
+        // [ addr.hrp = "ts" | separator = 1 | version = q | data = q79hzunlkj50fvm7rxg3xetx4kml4e0am43htk ]
+        let addr = Bech32::from_str(data);
+
+        // if addr.hrp == "hs1" || addr.hrp == "ts1" || addr.hrp == "ss1" || addr.hrp == "rs1"
+
+        // Hard coded for test
+        dbg!(addr);
+        Address{
+            version: 0,
+            hash: Default::default(),
+            network: "testnet".to_string()
+        }
+
+        //TODO: Need to dynamically get the network
+        // let network = match addr.hrp {
+        //     "hs" => "mainnet",
+        //     "ts" => "testnet",
+        //     "rs" => "regtest",
+        //     "ss" => "simnet"
+        // }
+        //
+        // let network
     }
 }
 
-
+#[cfg(test)] // Printing the test 'cargo test -- --nocapture'
+mod tests {
+    // Allows use of all code above
+    use super::*;
+    #[test]
+    fn test_addr_from_str() {
+        let addr = Address::from("ts1qq79hzunlkj50fvm7rxg3xetx4kml4e0am43htk");
+    }
+}
